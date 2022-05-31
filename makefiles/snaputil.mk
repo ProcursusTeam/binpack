92c35d0cae33fade588cf98e03bced4233836981
Cameron Katri <me@cameronkatri.com>
2022-05-31 15:30:58 -0700

    Update openssl and snaputil

--- makefiles/snaputil.mk
+++ makefiles/snaputil.mk
@@ -3,11 +3,11 @@ $(error Use the main Makefile)
 endif
 
 STRAPPROJECTS    += snaputil
-SNAPUTIL_VERSION := 10.15.1
+SNAPUTIL_VERSION := 12.3
 DEB_SNAPUTIL_V   ?= $(SNAPUTIL_VERSION)
 
 snaputil-setup: setup
-	$(call GITHUB_ARCHIVE,Diatrus,apfs,$(SNAPUTIL_VERSION),v$(SNAPUTIL_VERSION),snaputil)
+	$(call GITHUB_ARCHIVE,ahl,apfs,$(SNAPUTIL_VERSION),v$(SNAPUTIL_VERSION),snaputil)
 	$(call EXTRACT_TAR,snaputil-$(SNAPUTIL_VERSION).tar.gz,apfs-$(SNAPUTIL_VERSION),snaputil)
 	mkdir -p $(BUILD_STAGE)/snaputil/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin
 
@@ -16,7 +16,7 @@ snaputil:
 	@echo "Using previously built snaputil."
 else
 snaputil: snaputil-setup
-	$(CC) -arch $(MEMO_ARCH) -Os -Wall -isysroot $(TARGET_SYSROOT) $(PLATFORM_VERSION_MIN) -o $(BUILD_STAGE)/snaputil/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/snaputil $(BUILD_WORK)/snaputil/snapUtil.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/snaputil/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/snaputil $(BUILD_WORK)/snaputil/snapUtil.c -framework CoreFoundation -framework IOKit
 	$(call AFTER_BUILD)
 	$(call BINPACK_SIGN,snaputil.xml)
 endif
