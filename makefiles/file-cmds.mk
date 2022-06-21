561334d9d367bc60f6ba0424a01dc59914175047
m1stadev <adamhamdi31@gmail.com>
2022-06-21 12:14:42 -0500

    Only build xattr for iOS 13+

--- makefiles/file-cmds.mk
+++ makefiles/file-cmds.mk
@@ -6,7 +6,7 @@ ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 
 STRAPPROJECTS     += file-cmds
 # Don't upgrade file-cmds, as any future version includes APIs introduced in iOS 13+.
-ifeq ($(shell [ "$(CFVER_WHOLE)" -lt 1600 ] && echo 1),1)
+ifeq ($(shell [ "$(CFVER_WHOLE)" -le 1500 ] && echo 1),1)
 FILE-CMDS_VERSION := 272.250.1
 else
 FILE-CMDS_VERSION := 353.100.22
@@ -24,7 +24,9 @@ else
 file-cmds: file-cmds-setup
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)/sbin/mknod $(BUILD_WORK)/file-cmds/mknod/mknod.c
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/chflags $(BUILD_WORK)/file-cmds/chflags/chflags.c
+ifeq ($(shell [ "$(CFVER_WHOLE)" -gt 1500 ] && echo 1),1) # xattr is only available in file-cmds 352.40.66 and up
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/xattr $(BUILD_WORK)/file-cmds/xattr/xattr.c
+endif
 	$(call AFTER_BUILD)
 	$(call BINPACK_SIGN,general.xml)
 endif
