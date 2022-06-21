47714661c907de1523cc9e829f272adb31ff3aa4
m1stadev <adamhamdi31@gmail.com>
2022-06-21 12:15:06 -0500

    Update trustcache (previously tc)

---
+++ makefiles/trustcache.mk
@@ -0,0 +1,23 @@
+ifneq ($(PROCURSUS),1)
+$(error Use the main Makefile)
+endif
+
+SUBPROJECTS += trustcache
+TRUSTCACHE_VERSION  := 1.0
+
+trustcache-setup: setup
+	$(call GITHUB_ARCHIVE,CRKatri,trustcache,$(TRUSTCACHE_VERSION),v$(TRUSTCACHE_VERSION))
+	$(call EXTRACT_TAR,trustcache-$(TRUSTCACHE_VERSION).tar.gz,trustcache-$(TRUSTCACHE_VERSION),trustcache)
+
+ifneq ($(wildcard $(BUILD_WORK)/trustcache/.build_complete),)
+trustcache:
+	@echo "Using previously built trustcache."
+else
+trustcache: trustcache-setup
+	+$(MAKE) -C $(BUILD_WORK)/trustcache install \
+		PREFIX="$(BUILD_STAGE)/trustcache/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)"
+	$(call AFTER_BUILD)
+	$(call BINPACK_SIGN,general.xml)
+endif
+
+.PHONY: trustcache
