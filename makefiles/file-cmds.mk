7d5bbe1338ba282a84358b584cbe44d148897b27
Cameron Katri <me@cameronkatri.com>
2022-12-28 19:14:51 -0800

    adv-cmds: 400

--- makefiles/file-cmds.mk
+++ makefiles/file-cmds.mk
@@ -5,12 +5,7 @@ endif
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 
 STRAPPROJECTS     += file-cmds
-# Don't upgrade file-cmds, as any future version includes APIs introduced in iOS 13+.
-ifeq ($(shell [ "$(CFVER_WHOLE)" -le 1500 ] && echo 1),1)
-FILE-CMDS_VERSION := 272.250.1
-else
-FILE-CMDS_VERSION := 353.100.22
-endif
+FILE-CMDS_VERSION := 400
 
 file-cmds-setup: setup binpack-setup
 	$(call GITHUB_ARCHIVE,apple-oss-distributions,file_cmds,$(FILE-CMDS_VERSION),file_cmds-$(FILE-CMDS_VERSION))
@@ -24,7 +19,7 @@ file-cmds:
 	@echo "Using previously built file-cmds."
 else
 file-cmds: file-cmds-setup bzip2 xz
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)/sbin/mknod $(BUILD_WORK)/file-cmds/mknod/mknod.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)/sbin/mknod $(BUILD_WORK)/file-cmds/mknod/{mknod,pack_dev}.c
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/chflags $(BUILD_WORK)/file-cmds/chflags/chflags.c
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/sbin/chown $(BUILD_WORK)/file-cmds/chown/chown.c
 	for tool in chmod cp dd ln ls mkdir mv rm rmdir; do \
@@ -46,9 +41,7 @@ file-cmds: file-cmds-setup bzip2 xz
 		$(CC) $(CFLAGS) $(LDFLAGS) $$EXTRA_CFLAGS -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/$$tool $(BUILD_WORK)/file-cmds/$$tool/$$tool.c -D'__FBSDID(x)=' -D__POSIX_C_SOURCE; \
 	done
 	$(LN_S) gzip $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/gunzip
-ifeq ($(shell [ "$(CFVER_WHOLE)" -gt 1500 ] && echo 1),1) # xattr is only available in file-cmds 352.40.66 and up
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/xattr $(BUILD_WORK)/file-cmds/xattr/xattr.c
-endif
 	$(call AFTER_BUILD)
 	$(call BINPACK_SIGN,general.xml)
 	$(LDID) -Hsha256 -S$(BUILD_MISC)/entitlements/dd.xml $(BUILD_STAGE)/file-cmds/$(MEMO_PREFIX)/bin/dd
