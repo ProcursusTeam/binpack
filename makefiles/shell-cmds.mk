acb5abc3768e5a84bedcd8fbba3e446fb2801579
Cameron Katri <me@cameronkatri.com>
2022-12-28 19:44:02 -0800

    shell-cmds: 278
    Add env and realpath

--- makefiles/shell-cmds.mk
+++ makefiles/shell-cmds.mk
@@ -5,11 +5,11 @@ endif
 ifeq (,$(findstring darwin,$(MEMO_TARGET)))
 
 STRAPPROJECTS      += shell-cmds
-SHELL-CMDS_VERSION := 207.40.1
+SHELL-CMDS_VERSION := 278
 
 shell-cmds-setup: setup binpack-setup
-	$(call DOWNLOAD_FILES,$(BUILD_SOURCE),https://opensource.apple.com/tarballs/shell_cmds/shell_cmds-$(SHELL-CMDS_VERSION).tar.gz)
-	$(call EXTRACT_TAR,shell_cmds-$(SHELL-CMDS_VERSION).tar.gz,shell_cmds-$(SHELL-CMDS_VERSION),shell-cmds)
+	$(call GITHUB_ARCHIVE,apple-oss-distributions,shell_cmds,$(SHELL-CMDS_VERSION),shell_cmds-$(SHELL-CMDS_VERSION))
+	$(call EXTRACT_TAR,shell_cmds-$(SHELL-CMDS_VERSION).tar.gz,shell_cmds-shell_cmds-$(SHELL-CMDS_VERSION),shell-cmds)
 	mkdir -p $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX){,$(MEMO_SUB_PREFIX)}/bin
 
 ifneq ($(wildcard $(BUILD_WORK)/shell-cmds/.build_complete),)
@@ -23,7 +23,7 @@ shell-cmds: shell-cmds-setup
 	done; \
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/id id/id.c -D'__FBSDID(x)='; \
 	yacc -o $(BUILD_WORK)/shell-cmds/find/getdate.c $(BUILD_WORK)/shell-cmds/find/getdate.y; \
-	for bin in false find hexdump killall nohup printf pwd renice script seq tee time true uname w what which xargs; do \
+	for bin in env false find hexdump killall nohup printf pwd realpath renice script seq tee time true uname w what which xargs; do \
 		$(CC) $(CFLAGS) $(LDFLAGS) -o $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)$(MEMO_SUB_PREFIX)/bin/$$bin $$bin/*.c -D'__FBSDID(x)=' -DHAVE_UTMPX; \
 	done
 	$(LN_S) ..$(MEMO_SUB_PREFIX)/bin/pwd $(BUILD_STAGE)/shell-cmds/$(MEMO_PREFIX)/bin/pwd
